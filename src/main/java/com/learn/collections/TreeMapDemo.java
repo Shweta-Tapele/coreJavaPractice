4fa89cf228f06a69dbd5631305a494f8111509bf
Shweta-Tapele <stapele@pdx.edu>
2023-05-10 16:16:54 -0700

    Treeset

---
+++ src/main/java/com/learn/collections/TreeMapDemo.java
@@ -0,0 +1,15 @@
+package com.learn.collections;
+
+import java.util.TreeSet;
+
+public class TreeMapDemo {
+            public static void main(String[] args)
+            {
+                TreeSet t= new TreeSet();
+                t.add("A");
+                t.add("a");
+                t.add("B");
+                //t.add(new Integer(10));
+                System.out.println(t);
+            }
+}
