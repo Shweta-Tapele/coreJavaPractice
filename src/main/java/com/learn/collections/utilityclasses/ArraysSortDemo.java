b45c186b3df76452d3f22a746f06c75f7f5301fd
Shweta-Tapele <stapele@pdx.edu>
2023-05-16 13:08:22 -0700

    ArraysSorting

---
+++ src/main/java/com/learn/collections/utilityclasses/ArraysSortDemo.java
@@ -0,0 +1,66 @@
+package com.learn.collections.utilityclasses;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArraysSortDemo {
+
+    public static  void main(String[] args){
+        int a[] = {10,24,5,34,21,9};
+        System.out.println("Arrays before sorting");
+        for(int a1 : a)
+        {
+            System.out.println(a1);
+        }
+        Arrays.sort(a);
+        System.out.println("Arrays after sorting");
+        for(int a1 : a)
+        {
+            System.out.println(a1);
+        }
+
+        String[]  s ={"A", "N", "X", "W" };
+        Arrays.sort(s);
+        System.out.println("Arrays before sorting");
+        for(String s1 : s)
+        {
+            System.out.println(s1);
+        }
+        Arrays.sort(s);
+        System.out.println("Arrays after sorting");
+        for(String s1 : s)
+        {
+            System.out.println(s1);
+        }
+        System.out.println("Arrays before sorting");
+        for(String s2 : s)
+        {
+            System.out.println(s2);
+        }
+        Arrays.sort(s, new MyComparator());
+        System.out.println("Arrays after sorting");
+        for(String s2 : s)
+        {
+            System.out.println(s2);
+        }
+
+
+
+
+    }
+
+
+
+
+}
+
+
+class MyComparator implements  Comparator
+{
+    @Override
+    public int compare(Object o1, Object o2) {
+        String s1 = o1.toString();
+        String s2 = o2.toString();
+         return s2.compareTo(s1);
+    }
+}
