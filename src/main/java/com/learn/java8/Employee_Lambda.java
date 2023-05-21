5b6dea3e29e31b8b45da779f5f060bf2bbbd58b3
Shweta Tapele <shwetatapele@Shwetas-MacBook-Air.local>
2023-05-20 22:25:13 -0700

    Example of Predicate

---
+++ src/main/java/com/learn/java8/Employee_Lambda.java
@@ -0,0 +1,13 @@
+/* In collections to implement sorting we can use lambda expressions
+* We can use lambda expressions wherever we have functional Interface
+* */
+
+
+package com.learn.java8;
+
+public class Employee_Lambda {
+}
+
+
+
+//Predicate<String> p1=s->s.length() >5
---
+++ src/main/java/com/learn/java8/ExamPredicate.java
@@ -0,0 +1,35 @@
+package com.learn.java8;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+class Employee{
+        String name;
+        double salary;
+
+    public Employee(String name,double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
+public class ExamPredicate {
+
+        public static void main(String[] args)
+        {
+            ArrayList<Employee> l= new ArrayList<>();
+            l.add(new Employee("Shweta",8000));
+            l.add(new Employee("Leo",9000));
+            l.add(new Employee("Lee",3000));
+
+            Predicate<Employee> p= a-> a.salary >5000;
+            for(Employee e1: l)
+            {
+                if(p.test(e1)){
+                    System.out.println(e1.name+" "+e1.salary);
+                }
+            }
+        }
+
+}
+
+
